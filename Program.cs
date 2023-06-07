841f89e18a5c3a06048b615a098031130ee7a228
edu1107zera <128429561+edu1107zera@users.noreply.github.com>
2023-06-07 14:47:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula01Txt
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bem-Vindo as Cafeteriav  do \"Eduzera\"  \n\n\n\n");//titulo
+
+            Console.WriteLine("Essas sao as opçoes de hoje:");//subtitulo 
+            Console.WriteLine("-1 Cafe R$ 2,00");
+            Console.WriteLine("-2 Capuccino R$ 3,50");
+            Console.WriteLine("-3 Moccacino R$ 2,000,00  \n\n\n\n");
+
+            Console.WriteLine("Escolha uma opçao e dirija-se ao caixa....");//texto final
+
+            Console.ReadLine();
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
